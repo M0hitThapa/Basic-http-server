8643ee515173a84b4c39f716c8425f5c61f8930e
Mohit Thapa <mohitthapa229@gmail.com>
2024-06-02 23:57:09 +0545

    wrote node js code using express js

--- httpserver.js
+++ httpserver.js
@@ -1,34 +1,16 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
 
-const myServer = http.createServer((req,res) => {
-    const log = `${Date.now()}: ${req.url} i have reached bro\n`;
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-   fs.appendFile("logdetails.txt", log, (err,) => {
-    if (err) {
-        console.log("failed to write to log file", err);
-        res.writeHead(500,{"content-Type": "text/plain"});
-        res.end("Internal Server Error");
-        return;
-    }
-    res.writeHead(200, {"Content-Type": "text/html"});
-    switch (myUrl.pathname) {
-        case "/":
-            res.end("HomePage");
-            break;
-            case "/about":
-                const username = myUrl.query.myname
 
-                res.end(`hi, ${username}`);
-                break;
-                default:
-                    res.end("404 not Found");
-    }
-    
+const express = require("express");
 
-      
-   });
+const app = express();
+
+app.get('/',(req,res) => {
+    return res.send('Hello From Home Page');
 });
-myServer.listen(3000, () => console.log("Server Started man go to web"));
+
+app.get("/about", (req,res) => {
+    return res.send("Hello From About Page" + 'hey' + req.query.name + 'you are' + req.query.age);
+});
+
+app.listen(4000, () => console.log("server Started"));
+
